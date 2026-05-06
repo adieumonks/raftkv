390f0f532e5cb782f4d4721bb4c608dd41de4788
sudoken <44742486+adieumonks@users.noreply.github.com>
2026-05-06 15:58:10 +0900

    feat: add kv api types

---
+++ api/api.go
@@ -0,0 +1,99 @@
+package api
+
+type Response interface {
+	Status() ResponseStatus
+}
+
+type PutRequest struct {
+	Key   string
+	Value string
+
+	ClientID  int64
+	RequestID int64
+}
+
+type PutResponse struct {
+	RespStatus ResponseStatus
+	KeyFound   bool
+	PrevValue  string
+}
+
+func (pr *PutResponse) Status() ResponseStatus {
+	return pr.RespStatus
+}
+
+type AppendRequest struct {
+	Key   string
+	Value string
+
+	ClientID  int64
+	RequestID int64
+}
+
+type AppendResponse struct {
+	RespStatus ResponseStatus
+	KeyFound   bool
+	PrevValue  string
+}
+
+func (ar *AppendResponse) Status() ResponseStatus {
+	return ar.RespStatus
+}
+
+type GetRequest struct {
+	Key string
+
+	ClientID  int64
+	RequestID int64
+}
+
+type GetResponse struct {
+	RespStatus ResponseStatus
+	KeyFound   bool
+	Value      string
+}
+
+func (gr *GetResponse) Status() ResponseStatus {
+	return gr.RespStatus
+}
+
+type CASRequest struct {
+	Key          string
+	CompareValue string
+	Value        string
+
+	ClientID  int64
+	RequestID int64
+}
+
+type CASResponse struct {
+	RespStatus ResponseStatus
+	KeyFound   bool
+	PrevValue  string
+}
+
+func (cr *CASResponse) Status() ResponseStatus {
+	return cr.RespStatus
+}
+
+type ResponseStatus int
+
+const (
+	StatusInvalid ResponseStatus = iota
+	StatusOK
+	StatusNotLeader
+	StatusFailedCommit
+	StatusDuplicateRequest
+)
+
+var responseName = map[ResponseStatus]string{
+	StatusInvalid:          "Invalid",
+	StatusOK:               "OK",
+	StatusNotLeader:        "NotLeader",
+	StatusFailedCommit:     "FailedCommit",
+	StatusDuplicateRequest: "DuplicateRequest",
+}
+
+func (rs ResponseStatus) String() string {
+	return responseName[rs]
+}
